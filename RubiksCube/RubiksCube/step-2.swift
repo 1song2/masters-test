14fe5fa1f5c24fdede61f41034b5daf43dce6433
1song2 <1song2918@gmail.com>
2020-12-07 19:06:51 +0900

    Split the turnCube() function into smaller functions

--- RubiksCube/RubiksCube/step-2.swift
+++ RubiksCube/RubiksCube/step-2.swift
@@ -20,93 +20,77 @@ struct FlatCube {
         }
     }
     
-    mutating func turnCube(_ moveNotation: String) {
+    mutating func turnCube(_ moveNotation: String) -> [[String]]? {
         switch moveNotation {
-        /// 가장 윗줄을 왼쪽으로 한 칸 밀기
-        case "U":
-            movedCube[0].append(movedCube[0].removeFirst())
-            print()
-            print("U")
-            printCube(movedCube)
-        /// 가장 윗줄을 오른쪽으로 한 칸 밀기
-        case "U'":
-            movedCube[0].insert(movedCube[0].removeLast(), at: 0)
-            print()
-            print("U'")
-            printCube(movedCube)
-        /// 가장 오른쪽 줄을 위로 한 칸 밀기
-        case "R":
-            for index in 0..<movedCube.count {
-                if index - 1 < 0 {
-                    movedCube[movedCube.count - 1][movedCube.count - 1] = movedCube[index][movedCube.count - 1]
-                } else if index == 2 {
-                    movedCube[movedCube.count - 2][movedCube.count - 1] = originalCube[originalCube.count - 1][originalCube.count - 1]
-                } else {
-                    movedCube[index - 1][movedCube.count - 1] = movedCube[index][movedCube.count - 1]
-                }
-            }
-            print()
-            print("R")
-            printCube(movedCube)
-        /// 가장 오른쪽 줄을 아래로 한 칸 밀기
-        case "R'":
-            //for index in 0..<movedCube.count {
-            for index in stride(from: movedCube.count - 1, through: 0, by: -1) {
-                if index + 1 > 2 {
-                    movedCube[0][movedCube.count - 1] = movedCube[index][movedCube.count - 1]
-                } else if index == 0 {
-                    movedCube[movedCube.count - 2][movedCube.count - 1] = originalCube[0][originalCube.count - 1]
-                } else {
-                    movedCube[index + 1][movedCube.count - 1] = movedCube[index][movedCube.count - 1]
-                }
-            }
-            print()
-            print("R'")
-            printCube(movedCube)
-        /// 가장 왼쪽 줄을 아래로 한 칸 밀기
-        case "L":
-            for index in stride(from: movedCube.count - 1, through: 0, by: -1) {
-                if index + 1 > 2 {
-                    movedCube[0][0] = movedCube[index][0]
-                } else if index == 0 {
-                    movedCube[movedCube.count - 2][0] = originalCube[0][0]
-                } else {
-                    movedCube[index + 1][0] = movedCube[index][0]
-                }
-            }
-            print()
-            print("L")
-            printCube(movedCube)
-        /// 가장 왼쪽 줄을 위로 한 칸 밀기
-        case "L'":
-            for index in 0..<movedCube.count {
-                if index - 1 < 0 {
-                    movedCube[movedCube.count - 1][0] = movedCube[index][0]
-                } else if index == 2 {
-                    movedCube[movedCube.count - 2][0] = originalCube[originalCube.count - 1][0]
-                } else {
-                    movedCube[index - 1][0] = movedCube[index][0]
-                }
-            }
-            print()
-            print("L'")
-            printCube(movedCube)
-        /// 가장 아랫줄을 오른쪽으로 한 칸 밀기
-        case "B":
-            movedCube[2].insert(movedCube[2].removeLast(), at: 0)
-            print()
-            print("B")
-            printCube(movedCube)
-        /// 가장 아랫줄을 왼쪽으로 한 칸 밀기
-        case "B'":
-            movedCube[2].append(movedCube[2].removeFirst())
-            print()
-            print("B'")
-            printCube(movedCube)
         case "Q":
             print("Bye👋")
+            return nil
+        /// 가장 윗줄을 왼쪽으로 한 칸 밀기
+        case "U":
+            turnLeft(row: 0)
+            return movedCube
+        /// 가장 윗줄을 오른쪽으로 한 칸 밀기
+        case "U'":
+            turnRight(row: 0)
+            return movedCube
+        /// 가장 오른쪽 줄을 위로 한 칸 밀기
+        case "R":
+            pushUp(column: movedCube.count - 1)
+            return movedCube
+        /// 가장 오른쪽 줄을 아래로 한 칸 밀기
+        case "R'":
+            pushDown(column: movedCube.count - 1)
+            return movedCube
+        /// 가장 왼쪽 줄을 아래로 한 칸 밀기
+        case "L":
+            pushDown(column: 0)
+            return movedCube
+        /// 가장 왼쪽 줄을 위로 한 칸 밀기
+        case "L'":
+            pushUp(column: 0)
+            return movedCube
+        /// 가장 아랫줄을 오른쪽으로 한 칸 밀기
+        case "B":
+            turnRight(row: 2)
+            return movedCube
+        /// 가장 아랫줄을 왼쪽으로 한 칸 밀기
+        case "B'":
+            turnLeft(row: 2)
+            return movedCube
         default:
-            break
+            return nil
+        }
+    }
+    
+    mutating func turnLeft(row: Int) {
+        movedCube[row].append(movedCube[row].removeFirst())
+    }
+    
+    mutating func turnRight(row: Int) {
+        movedCube[row].insert(movedCube[row].removeLast(), at: 0)
+    }
+    
+    mutating func pushUp(column: Int) {
+        for index in 0..<movedCube.count {
+            if index - 1 < 0 {
+                movedCube[movedCube.count - 1][column] = movedCube[index][column]
+            } else if index == 2 {
+                movedCube[movedCube.count - 2][column] = originalCube[originalCube.count - 1][column]
+            } else {
+                movedCube[index - 1][column] = movedCube[index][column]
+            }
+        }
+    }
+    
+    mutating func pushDown(column: Int) {
+        for index in stride(from: movedCube.count - 1, through: 0, by: -1) {
+            if index + 1 > 2 {
+                movedCube[0][column] = movedCube[index][column]
+            } else if index == 0 {
+                movedCube[movedCube.count - 2][column] = originalCube[0][column]
+            } else {
+                movedCube[index + 1][column] = movedCube[index][column]
+            }
         }
     }
 }
